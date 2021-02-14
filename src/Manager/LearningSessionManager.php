4f5113c22cd17d1dc64aec7d4bd159d85af831ce
Simon Balluais <simon.ballu@gmail.com>
2021-02-14 22:50:14 +0300

    Fix serie counter page

--- src/Manager/LearningSessionManager.php
+++ src/Manager/LearningSessionManager.php
@@ -217,8 +217,6 @@ class LearningSessionManager
             $end
         );
 
-        var_dump($lsCountToday);
-
         if (!$lsCountToday) {
             $user->incrementCurrentSerie();
         }
